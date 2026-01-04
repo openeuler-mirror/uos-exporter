b583acc7526298743534e51b0092d210ee08dedc
liweiganga <liweiganga@uniontech.com>
2026-01-05 07:43:00 +0800

    Add initial structure for postgres_exporter/internal/collector/extension_collector.go

---
+++ postgres_exporter/internal/collector/extension_collector.go
@@ -0,0 +1,13 @@
+package collector
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"postgres_exporter/internal/model"
+)
+
+// ScrapePostgreSQLExtensions 采集当前数据库中已安装的扩展列表
+
+// TODO: implement functions
