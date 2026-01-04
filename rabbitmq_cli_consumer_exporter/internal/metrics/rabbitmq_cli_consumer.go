f359a28b496458bebeb7519a6b4d4f6c222eae68
liangyangang <liangyangang@uniontech.com>
2026-01-05 01:10:00 +0800

    Add initial structure for rabbitmq_cli_consumer_exporter/internal/metrics/rabbitmq_cli_consumer.go

---
+++ rabbitmq_cli_consumer_exporter/internal/metrics/rabbitmq_cli_consumer.go
@@ -0,0 +1,15 @@
+package metrics
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"rabbitmq_cli_consumer_exporter/internal/exporter"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
+)
+
+
+// TODO: implement functions
