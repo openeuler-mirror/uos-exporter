e5f8940845670206f5a88526795fb7622aab53b1
liangyangang <liangyangang@uniontech.com>
2026-01-05 03:03:00 +0800

    Add initial structure for prometheus_html_exporter/internal/metrics/config_test.go

---
+++ prometheus_html_exporter/internal/metrics/config_test.go
@@ -0,0 +1,15 @@
+package metrics
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// TestLoadConfig 测试加载配置功能
+
+// TODO: implement functions
