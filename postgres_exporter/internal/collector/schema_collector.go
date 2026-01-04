1dc179a705a2a1057eb048ea5f3172f3e1ae200b
liweiganga <liweiganga@uniontech.com>
2026-01-05 07:55:00 +0800

    Add initial structure for postgres_exporter/internal/collector/schema_collector.go

---
+++ postgres_exporter/internal/collector/schema_collector.go
@@ -0,0 +1,14 @@
+package collector
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"postgres_exporter/internal/model"
+)
+
+// ScrapePostgreSQLSchemas 采集当前数据库中所有非系统 Schema 的信息
+
+// TODO: implement functions
