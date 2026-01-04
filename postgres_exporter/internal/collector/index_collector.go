2db0c28176b69fed49d3167cdcbfe659f0312a5b
liweiganga <liweiganga@uniontech.com>
2026-01-05 07:45:00 +0800

    Add initial structure for postgres_exporter/internal/collector/index_collector.go

---
+++ postgres_exporter/internal/collector/index_collector.go
@@ -0,0 +1,13 @@
+package collector
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"postgres_exporter/internal/model"
+)
+
+// ScrapePostgreSQLIndexes 使用你提供的 SQL 查询采集索引数据
+
+// TODO: implement functions
