350e6c80d67e7832c64f41152c2c6113037c8530
liweiganga <liweiganga@uniontech.com>
2026-01-05 07:39:00 +0800

    Add initial structure for postgres_exporter/internal/collector/bgwriter_collector.go

---
+++ postgres_exporter/internal/collector/bgwriter_collector.go
@@ -0,0 +1,13 @@
+package collector
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"postgres_exporter/internal/model"
+)
+
+// ScrapePostgreSQLBGWriter 采集后台写入器指标
+
+// TODO: implement functions
