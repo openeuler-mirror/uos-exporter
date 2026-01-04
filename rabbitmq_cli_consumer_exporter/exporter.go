7ddeb15a3f4b0cdacefd49d4d85b2bbc36519e7c
liangyangang <liangyangang@uniontech.com>
2026-01-05 00:04:00 +0800

    Add initial structure for rabbitmq_cli_consumer_exporter/exporter.go

---
+++ rabbitmq_cli_consumer_exporter/exporter.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"rabbitmq_cli_consumer_exporter/internal/server"
+	"rabbitmq_cli_consumer_exporter/pkg/logger"
+
+	"github.com/sirupsen/logrus"
+)
+
+
+// TODO: implement
