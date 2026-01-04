8bac3d8185a8c47ed35732f26726ed975e1c76e8
liweiganga <liweiganga@uniontech.com>
2026-01-05 07:53:00 +0800

    Add initial structure for postgres_exporter/internal/collector/role_collector.go

---
+++ postgres_exporter/internal/collector/role_collector.go
@@ -0,0 +1,13 @@
+package collector
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"postgres_exporter/internal/model"
+)
+
+// ScrapePostgreSQLRoles 采集所有 PostgreSQL 角色信息
+
+// TODO: implement functions
