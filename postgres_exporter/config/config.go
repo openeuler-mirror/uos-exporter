33f72f0a24ef1785587150fdf3cf817f2e5d09f8
liweiganga <liweiganga@uniontech.com>
2026-01-05 07:40:00 +0800

    Add initial structure for postgres_exporter/config/config.go

---
+++ postgres_exporter/config/config.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"github.com/alecthomas/kingpin"
+	"github.com/sirupsen/logrus"
+	"postgres_exporter/pkg/utils"
+)
+
+var (
+	ScrapeUrl       *string
+	Insecure        *bool
+	DefaultSettings = Settings{
+		// ScrapeUri: "http://127.0.0.1:24220/api/plugins.json",
+		Insecure: false,
+	}
+)
+
+func init() {
+	ScrapeUrl = kingpin.Flag("scrape_uri",
+		"Scrape URI").
+		Short('s').
