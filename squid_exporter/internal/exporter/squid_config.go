f7126fa508cd78ca66a872052389bd84126f5a3c
liangyangang <liangyangang@uniontech.com>
2026-01-05 01:56:00 +0800

    Add initial structure for squid_exporter/internal/exporter/squid_config.go

---
+++ squid_exporter/internal/exporter/squid_config.go
@@ -0,0 +1,11 @@
+package exporter
+
+import (
+	"squid_exporter/config"
+	"squid_exporter/internal/metrics"
+	"github.com/sirupsen/logrus"
+)
+
+// InitSquidCollector 初始化Squid收集器
+
+// TODO: implement functions
