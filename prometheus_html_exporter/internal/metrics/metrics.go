e93b39c38a0b4d8ef094ab33c1a3744a893835dc
liangyangang <liangyangang@uniontech.com>
2026-01-05 03:13:00 +0800

    Add initial structure for prometheus_html_exporter/internal/metrics/metrics.go

---
+++ prometheus_html_exporter/internal/metrics/metrics.go
@@ -0,0 +1,15 @@
+package metrics
+
+import (
+	"prometheus_html_exporter/internal/exporter"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	Name    = "prometheus-html-exporter"
+	Version = "1.0.0"
+)
+
+// Register 注册一个指标收集器
+
+// TODO: implement functions
