580438d4d129c3dc657f5660a6fb65d6266e4033
liangyangang <liangyangang@uniontech.com>
2026-01-05 03:44:00 +0800

    Add initial structure for iptables_exporter/config/config.go

---
+++ iptables_exporter/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"iptables_exporter/pkg/utils"
+
+	"github.com/alecthomas/kingpin"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	ScrapeUrl       *string
+	Insecure        *bool
+	DefaultSettings = Settings{
+		//ScrapeUri: "http://127.0.0.1:24220/api/plugins.json",
+		Insecure: false,
+	}
+)
+
+func init() {
+	ScrapeUrl = kingpin.Flag("scrape_uri",
+		"Scrape URI").
+		Short('s').
+		String()
+	Insecure = kingpin.Flag("insecure",
+		"Ignore server certificate if using https, Default: false.").
+		Bool()
+	if *ScrapeUrl != "" {
+		if err := utils.ValidateURI(*ScrapeUrl); err != nil {
+			logrus.Warnf("Invalid scrape uri: %s", err)
+			logrus.Warnf("Use default scrape uri: %s", DefaultSettings.ScrapeUri)
+			*ScrapeUrl = DefaultSettings.ScrapeUri
+		}
+	}
+
+	if *Insecure {
+		logrus.Warn("Insecure mode enabled, this is not recommended for production use.")
+	}
+}
+
+type Settings struct {
+	ScrapeUri string `yaml:"scrape_uri"`
+	Insecure  bool   `yaml:"insecure"`
+}
