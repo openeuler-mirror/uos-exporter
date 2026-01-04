7b5aebbacaad2adfe2e8a7e3d32f2a206428723a
liangyangang <liangyangang@uniontech.com>
2026-01-05 03:51:00 +0800

    Add initial structure for iptables_exporter/internal/exporter/config.go

---
+++ iptables_exporter/internal/exporter/config.go
@@ -0,0 +1,59 @@
+package exporter
+
+import (
+	"iptables_exporter/pkg/logger"
+	"iptables_exporter/pkg/utils"
+	"os"
+	"time"
+
+	"github.com/alecthomas/kingpin"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
+)
+
+var (
+	Configfile    *string
+	DefaultConfig = Config{
+		Logging: logger.Config{
+			Level:   "debug",
+			LogPath: "/var/log/exporter.log",
+			MaxSize: "10MB",
+			MaxAge:  time.Hour * 24 * 7},
+		Address:     "127.0.0.1",
+		Port:        8080,
+		MetricsPath: "/metrics",
+	}
+)
+
+func init() {
+	kingpin.HelpFlag.Short('h')
+	Configfile = kingpin.Flag("config", "Configuration file").
+		Short('c').
+		Default("/etc/uos-exporter/iptables-exporter.yaml").
+		String()
+}
+
+type Config struct {
+	Logging     logger.Config `yaml:"log"`
+	Address     string        `yaml:"address"`
+	Port        int           `yaml:"port"`
+	MetricsPath string        `yaml:"metricsPath"`
+}
+
+func Unpack(config interface{}) error {
+	if !utils.FileExists(*Configfile) {
+		logrus.Errorf("%s file not found", *Configfile)
+		logrus.Debug("Use default config")
+	} else {
+		file, err := os.Open(*Configfile)
+		if err != nil {
+			logrus.Error("Failed to open config file: ", err)
+			return err
+		}
+		err = yaml.NewDecoder(file).Decode(config)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
