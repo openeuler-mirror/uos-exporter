a962a69cca05cbaf4199581062288d3da5ed5a6b
lvgenggeng <lvgenggeng@uniontech.com>
2026-01-05 00:12:00 +0800

    Add initial structure for rabbitmq_cli_consumer_exporter/pkg/utils/cmd.go

---
+++ rabbitmq_cli_consumer_exporter/pkg/utils/cmd.go
@@ -0,0 +1,14 @@
+package utils
+
+import "os/exec"
+
+// #nosec G204
+func RunCommand(name string, args ...string) ([]byte, error) {
+	cmd := exec.Command(name, args...)
+	return cmd.Output()
+}
+
+// #nosec G204
+func GetCommand(name string, args ...string) *exec.Cmd {
+	return exec.Command(name, args...)
+}
