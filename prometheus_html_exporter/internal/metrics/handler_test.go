1bf5fb4e164ab3ffba0cbfefebaf972bd63a6d87
liangyangang <liangyangang@uniontech.com>
2026-01-05 03:07:00 +0800

    Add initial structure for prometheus_html_exporter/internal/metrics/handler_test.go

---
+++ prometheus_html_exporter/internal/metrics/handler_test.go
@@ -0,0 +1,17 @@
+package metrics
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// TestProbeHandler 测试ProbeHandler功能
+
+// TODO: implement functions
