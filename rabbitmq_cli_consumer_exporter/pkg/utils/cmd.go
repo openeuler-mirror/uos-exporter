111a679f4926a09905a5a2fdc7c88901dd03b68c
lvgenggeng <lvgenggeng@uniontech.com>
2026-01-05 00:12:00 +0800

    Complete implementation of rabbitmq_cli_consumer_exporter/pkg/utils/cmd.go

--- rabbitmq_cli_consumer_exporter/pkg/utils/cmd.go
+++ rabbitmq_cli_consumer_exporter/pkg/utils/cmd.go
@@ -12,3 +12,4 @@ func RunCommand(name string, args ...string) ([]byte, error) {
 func GetCommand(name string, args ...string) *exec.Cmd {
 	return exec.Command(name, args...)
 }
+// Part 2 commit for rabbitmq_cli_consumer_exporter/pkg/utils/cmd.go
