c3ae5eee11c5dbeece29581b3e12aecf5c9797f1
liweiganga <liweiganga@uniontech.com>
2026-01-05 07:41:00 +0800

    Add initial structure for postgres_exporter/internal/collector/bloat_collector.go

---
+++ postgres_exporter/internal/collector/bloat_collector.go
@@ -0,0 +1,13 @@
+package collector
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"postgres_exporter/internal/model"
+)
+
+// ScrapePostgreSQLBloat 采集所有表和索引的膨胀信息（兼容 PG 15+）
+
+// TODO: implement functions
