237f34d439a440ef776973d4a300d0512119cfaa
liangyangang <liangyangang@uniontech.com>
2026-01-05 00:01:00 +0800

    Add initial structure for rabbitmq_cli_consumer_exporter/config/config.go

---
+++ rabbitmq_cli_consumer_exporter/config/config.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"rabbitmq_cli_consumer_exporter/pkg/utils"
+
+	"github.com/alecthomas/kingpin"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	ScrapeUrl       *string
+	Insecure        *bool
+	DefaultSettings = Settings{
+		//ScrapeUri: "http://127.0.0.1:24220/api/plugins.json",
+		Insecure: false,
+	}
+)
+
+func init() {
+	ScrapeUrl = kingpin.Flag("scrape_uri",
+		"Scrape URI").
+		Short('s').
