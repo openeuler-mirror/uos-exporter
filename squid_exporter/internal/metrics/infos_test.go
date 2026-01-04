a4a42c2f048ab8ca6048a0283a0a35b4d1e55a70
liangyangang <liangyangang@uniontech.com>
2026-01-05 02:06:00 +0800

    Add initial structure for squid_exporter/internal/metrics/infos_test.go

---
+++ squid_exporter/internal/metrics/infos_test.go
@@ -0,0 +1,14 @@
+package metrics
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+// 测试信息解码函数
+
+// TODO: implement functions
