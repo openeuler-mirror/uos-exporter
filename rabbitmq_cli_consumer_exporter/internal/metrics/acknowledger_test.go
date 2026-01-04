e78ac1d4f08648ee15607919da6db379a6c6cf56
liangyangang <liangyangang@uniontech.com>
2026-01-05 00:15:00 +0800

    Add initial structure for rabbitmq_cli_consumer_exporter/internal/metrics/acknowledger_test.go

---
+++ rabbitmq_cli_consumer_exporter/internal/metrics/acknowledger_test.go
@@ -0,0 +1,16 @@
+package metrics
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type TestDelivery struct {
+	mock.Mock
+}
+
+
+// TODO: implement
