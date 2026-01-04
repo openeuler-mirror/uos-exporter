e4ec1cb99e33b301165ec0c16e65c5d85d58c56b
liangyangang <liangyangang@uniontech.com>
2026-01-05 00:40:00 +0800

    Add initial structure for rabbitmq_cli_consumer_exporter/internal/metrics/error.go

---
+++ rabbitmq_cli_consumer_exporter/internal/metrics/error.go
@@ -0,0 +1,28 @@
+package metrics
+
+import "fmt"
+
+// NewCreateCommandError creates a new CreateCommandError from the given error.
+func NewCreateCommandError(err error) error {
+	return &CreateCommandError{err}
+}
+
+// CreateCommandError defines an error indicating that the creation of a command failed.
+type CreateCommandError struct {
+	err error
+}
+
+// Error is part of the error builtin.
+func (e CreateCommandError) Error() string {
+	return fmt.Sprintf("failed to register a consumer: %v", e.err)
+}
+
+// NewAcknowledgmentError creates a new AcknowledgmentError from the given error.
+func NewAcknowledgmentError(err error) error {
+	return &AcknowledgmentError{err}
+}
+
+// AcknowledgmentError defines an error indicating that acknowledgment of a message failed.
+type AcknowledgmentError struct {
+
+// TODO: implement functions
