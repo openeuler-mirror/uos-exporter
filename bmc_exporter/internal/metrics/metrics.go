83ac4ce4b4203b7bb5bfd91d02e9947e635b6a09
lvgenggeng <lvgenggeng@uniontech.com>
2026-01-05 18:05:00 +0800

    Complete implementation of bmc_exporter/internal/metrics/metrics.go

--- bmc_exporter/internal/metrics/metrics.go
+++ bmc_exporter/internal/metrics/metrics.go
@@ -13,5 +13,21 @@ type baseMetrics struct {
 	desc *prometheus.Desc
 }
 
+func NewMetrics(fqname, help string, labels []string) *baseMetrics {
+	return &baseMetrics{
+		desc: prometheus.NewDesc(
+			fqname,
+			help,
+			labels,
+			nil,
+		),
+	}
+}
 
-// TODO: implement functions
+func (c *baseMetrics) Desc() *prometheus.Desc {
+	return c.desc
+}
+
+func (c *baseMetrics) collect(ch chan<- prometheus.Metric, value float64, labels []string) {
+	ch <- prometheus.MustNewConstMetric(c.desc, prometheus.GaugeValue, value, labels...)
+}
