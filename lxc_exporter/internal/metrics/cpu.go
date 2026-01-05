904c387650a029937cda9684f85522da6dddc3bd
mahailiang <mahailiang@uniontech.com>
2026-01-05 23:24:00 +0800

    Add initial structure for lxc_exporter/internal/metrics/cpu.go

---
+++ lxc_exporter/internal/metrics/cpu.go
@@ -0,0 +1,15 @@
+//go:build example
+
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/sirupsen/logrus"
+	"lxc_exporter/internal/exporter"
+	"os"
+	"strconv"
+	"strings"
+)
+
+
+// TODO: implement functions
