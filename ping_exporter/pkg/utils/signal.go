527026f7b33fc5fa862b51288a0f8a82ff1bad33
liweiganga <liweiganga@uniontech.com>
2026-01-05 10:16:00 +0800

    Add initial structure for ping_exporter/pkg/utils/signal.go

--- ping_exporter/pkg/utils/signal.go
+++ ping_exporter/pkg/utils/signal.go
@@ -8,15 +8,5 @@ import (
 	"syscall"
 )
 
-func HandleSignals(function func()) {
-	var callback sync.Once
-	sigc := make(chan os.Signal, 1)
-	defer close(sigc)
-	signal.Notify(sigc,
-		syscall.SIGINT,
-		syscall.SIGTERM)
 
-	sig := <-sigc
-	logrus.Infof("service received signal: %v", sig)
-	callback.Do(function)
-}
+// TODO: implement functions
