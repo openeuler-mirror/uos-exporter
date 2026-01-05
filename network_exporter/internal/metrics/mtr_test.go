c8cd99c33774d90db63a1aeaf1cc8ad87efc61c0
maqi <maqi@uniontech.com>
2026-01-06 06:51:00 +0800

    Add initial structure for network_exporter/internal/metrics/mtr_test.go

---
+++ network_exporter/internal/metrics/mtr_test.go
@@ -0,0 +1,14 @@
+package metrics
+
+import (
+	"log/slog"
+	"net"
+	"os"
+	"testing"
+
+	"network_exporter/config"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+
+// TODO: implement functions
