8411899e2013938f4fd2f3add4a88c08e699ae45
liweiganga <liweiganga@uniontech.com>
2026-01-05 10:39:00 +0800

    Add initial structure for node_utility_exporter/internal/metrics/os_release_test.go

---
+++ node_utility_exporter/internal/metrics/os_release_test.go
@@ -0,0 +1,25 @@
+package metrics
+
+import (
+	"errors"
+	"io"
+	"log/slog"
+	"os"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var timeParse = time.Parse
+
+type errorReader struct {
+	err error
+}
+
+
+// TODO: implement functions
