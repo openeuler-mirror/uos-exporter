59c54dfb71749452b2593c86a876b0ec4ba359d8
lvgenggeng <lvgenggeng@uniontech.com>
2026-01-05 15:48:00 +0800

    Add initial structure for hacluster_exporter/internal/metrics/collectors/pacemaker/crmmon/parser.go

---
+++ hacluster_exporter/internal/metrics/collectors/pacemaker/crmmon/parser.go
@@ -0,0 +1,33 @@
+package crmmon
+
+import (
+	"encoding/xml"
+	"hacluster_exporter/pkg/utils"
+
+	"github.com/pkg/errors"
+)
+
+type Parser interface {
+	Parse() (Root, error)
+}
+
+type crmMonParser struct {
+	crmMonPath string
+}
+
+func (c *crmMonParser) Parse() (crmMon Root, err error) {
+	crmMonXML, err := utils.RunCommand(c.crmMonPath, "-X", "--inactive")
+	if err != nil {
+		return crmMon, errors.Wrap(err, "error while executing crm_mon")
+	}
+
+	err = xml.Unmarshal(crmMonXML, &crmMon)
+	if err != nil {
+		return crmMon, errors.Wrap(err, "error while parsing crm_mon XML output")
+	}
+
+	return crmMon, nil
+}
+
+
+// TODO: implement functions
