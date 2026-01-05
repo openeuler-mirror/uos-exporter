31541ec645f9c6d84713b754678f3ec7e493dfb2
lvgenggeng <lvgenggeng@uniontech.com>
2026-01-05 15:15:00 +0800

    Add initial structure for hacluster_exporter/internal/clock/clock.go

---
+++ hacluster_exporter/internal/clock/clock.go
@@ -0,0 +1,27 @@
+package clock
+
+import "time"
+
+type Clock interface {
+	Now() time.Time
+	Since(t time.Time) time.Duration
+}
+
+type StoppedClock struct{}
+
+const TEST_TIMESTAMP = 1234
+
+func (StoppedClock) Now() time.Time {
+	ms := TEST_TIMESTAMP * time.Millisecond
+	return time.Date(1970, 1, 1, 0, 0, 0, int(ms.Nanoseconds()), time.UTC)
+	// 1234 millisecond after Unix epoch (1970-01-01 00:00:01.234 +0000 UTC)
+	// this will allow us to use a fixed timestamped when running assertions
+}
+
+func (StoppedClock) Since(t time.Time) time.Duration {
+	return TEST_TIMESTAMP * time.Millisecond
+}
+
+type SystemClock struct{}
+
+// TODO: implement functions
