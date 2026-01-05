0d2a71446c066ca77ba189b028b0b0ee67acd82a
maqi <maqi@uniontech.com>
2026-01-06 06:35:00 +0800

    Complete implementation of network_exporter/internal/exporter/config.go

--- network_exporter/internal/exporter/config.go
+++ network_exporter/internal/exporter/config.go
@@ -29,5 +29,48 @@ var (
 	}
 )
 
+func init() {
+	Configfile = kingpin.Flag("config", "Configuration file").
+		Short('c').
+		Default("/etc/uos-exporter/network-exporter.yaml").
+		String()
 
-// TODO: implement functions
+	// 添加兼容旧项目的命令行参数
+	NetworkConfigfile = kingpin.Flag("config.file", "Network exporter configuration file").
+		Default("/etc/uos-exporter/network-exporter.yaml").
+		String()
+}
+
+type Config struct {
+	Logging     logger.Config `yaml:"log"`
+	Address     string        `yaml:"address"`
+	Port        int           `yaml:"port"`
+	MetricsPath string        `yaml:"metricsPath"`
+}
+
+func Unpack(config interface{}) error {
+	configPath := *Configfile
+	if utils.FileExists(*NetworkConfigfile) {
+		// 如果使用了旧项目的命令行参数格式，优先使用它
+		configPath = *NetworkConfigfile
+	}
+	cleanPath := filepath.Clean(configPath)
+	configDir := "/etc/uos-exporter"
+	if !strings.HasPrefix(cleanPath, configDir) {
+		return fmt.Errorf("config file must be located within %s", configDir)
+	}
+	if !utils.FileExists(cleanPath) {
+		logrus.Errorf("%s file not found", configPath)
+	} else {
+		file, err := os.Open(configPath)
+		if err != nil {
+			logrus.Error("Failed to open config file: ", err)
+			return err
+		}
+		err = yaml.NewDecoder(file).Decode(config)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
