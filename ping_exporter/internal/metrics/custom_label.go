0370e96654630c24bce8837b9f46f31ca14885d7
liweiganga <liweiganga@uniontech.com>
2026-01-05 09:38:00 +0800

    Complete implementation of ping_exporter/internal/metrics/custom_label.go

--- ping_exporter/internal/metrics/custom_label.go
+++ ping_exporter/internal/metrics/custom_label.go
@@ -5,5 +5,54 @@ type customLabelSet struct {
 	nameMap map[string]interface{}
 }
 
+func newCustomLabelSet(targets []TargetConfig) *customLabelSet {
+	cl := &customLabelSet{
+		nameMap: make(map[string]interface{}),
+		names:   make([]string, 0),
+	}
 
-// TODO: implement functions
+	for _, t := range targets {
+		cl.addLabelsForTarget(&t)
+	}
+
+	return cl
+}
+
+func (cl *customLabelSet) addLabelsForTarget(t *TargetConfig) {
+	if t.Labels == nil {
+		return
+	}
+
+	for name := range t.Labels {
+		cl.addLabel(name)
+	}
+}
+
+func (cl *customLabelSet) addLabel(name string) {
+	_, exists := cl.nameMap[name]
+	if exists {
+		return
+	}
+
+	cl.names = append(cl.names, name)
+	cl.nameMap[name] = nil
+}
+
+func (cl *customLabelSet) labelNames() []string {
+	return cl.names
+}
+
+func (cl *customLabelSet) labelValues(t TargetConfig) []string {
+	values := make([]string, len(cl.names))
+	if t.Labels == nil {
+		return values
+	}
+
+	for i, name := range cl.names {
+		if value, isSet := t.Labels[name]; isSet {
+			values[i] = value
+		}
+	}
+
+	return values
+}
