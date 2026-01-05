548b745473519c07b838747a9e21f4c85f0e5de9
lvgenggeng <lvgenggeng@uniontech.com>
2026-01-05 15:23:00 +0800

    Complete implementation of hacluster_exporter/internal/metrics/collectors/core/default_collector.go

--- hacluster_exporter/internal/metrics/collectors/core/default_collector.go
+++ hacluster_exporter/internal/metrics/collectors/core/default_collector.go
@@ -20,5 +20,31 @@ type DefaultCollector struct {
 	timestamps bool
 }
 
+func NewDefaultCollector(subsystem string, timestamps bool) DefaultCollector {
+	return DefaultCollector{
+		subsystem,
+		&clock.SystemClock{},
+		timestamps,
+	}
+}
 
-// TODO: implement functions
+func (c *DefaultCollector) GetSubsystem() string {
+	return c.subsystem
+}
+
+// check that all the given paths exist and are executable files
+func CheckExecutables(paths ...string) error {
+	for _, path := range paths {
+		fileInfo, err := os.Stat(path)
+		if err != nil || os.IsNotExist(err) {
+			return errors.Errorf("'%s' does not exist", path)
+		}
+		if fileInfo.IsDir() {
+			return errors.Errorf("'%s' is a directory", path)
+		}
+		if (fileInfo.Mode() & 0111) == 0 {
+			return errors.Errorf("'%s' is not executable", path)
+		}
+	}
+	return nil
+}
