ae260bf356e0b5f32fbe63b3b9086a098e8c49bc
maqi <maqi@uniontech.com>
2026-01-06 07:13:00 +0800

    Add initial structure for network_exporter/pkg/common/types.go

---
+++ network_exporter/pkg/common/types.go
@@ -0,0 +1,51 @@
+package common
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+// IcmpID ICMP Echo Unique ID for each coroutine
+type IcmpID struct {
+	icmpID int32
+}
+
+// Get ICMP Echo Unique ID
+func (c *IcmpID) Get() int32 {
+	for {
+		val := atomic.LoadInt32(&c.icmpID)
+		// Init
+		if val == 0 {
+			atomic.StoreInt32(&c.icmpID, 1)
+			val = 1
+		}
+		// Reset Counter
+		if atomic.CompareAndSwapInt32(&c.icmpID, 65500, 2) {
+			return 1
+		}
+		if atomic.CompareAndSwapInt32(&c.icmpID, val, val+1) {
+			return val
+		}
+	}
+}
+
+// IcmpReturn ICMP Response time details
+type IcmpReturn struct {
+	Success bool
+	Addr    string
+	Elapsed time.Duration
+}
+
+// IcmpSummary ICMP HOP Summary
+type IcmpSummary struct {
+	AddressFrom string        `json:"address_from"`
+	AddressTo   string        `json:"address_to"`
+	Snt         int           `json:"snt"`
+	SntFail     int           `json:"snt_fail"`
+	SntTime     time.Duration `json:"snt_time"`
+}
+
+// IcmpHop ICMP HOP Response time details
+type IcmpHop struct {
+
+// TODO: implement functions
