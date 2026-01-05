36eb6e11731c97d65257148ea4b3f92b9e16983f
liweiganga <liweiganga@uniontech.com>
2026-01-05 09:43:00 +0800

    Add initial structure for ping_exporter/internal/metrics/ping_exporter.go

---
+++ ping_exporter/internal/metrics/ping_exporter.go
@@ -0,0 +1,16 @@
+package metrics
+
+import (
+	"os"
+	"ping_exporter/internal/exporter"
+	"strings"
+	"sync"
+
+	mon "github.com/digineo/go-ping/monitor"
+
+	"github.com/prometheus/client_golang/prometheus"
+	log "github.com/sirupsen/logrus"
+)
+
+
+// TODO: implement functions
