6a2f0c80ee0a2ea90b3d289f9b13ea6c8b779782
lvgenggeng <lvgenggeng@uniontech.com>
2026-01-05 16:40:00 +0800

    Add initial structure for node_network_exporter/config/config.go

---
+++ node_network_exporter/config/config.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"node_network_exporter/pkg/utils"
+	"github.com/alecthomas/kingpin"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	ScrapeUrl       *string
+	Insecure        *bool
+	DefaultSettings = Settings{
+		//ScrapeUri: "http://127.0.0.1:24220/api/plugins.json",
+		Insecure: false,
+	}
+)
+
+
+// TODO: implement functions
