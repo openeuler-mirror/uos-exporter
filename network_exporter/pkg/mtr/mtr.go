7f854564d07cd2cfd7ea405c5718ca5f9faeb478
maqi <maqi@uniontech.com>
2026-01-06 07:30:00 +0800

    Add initial structure for network_exporter/pkg/mtr/mtr.go

---
+++ network_exporter/pkg/mtr/mtr.go
@@ -0,0 +1,14 @@
+package mtr
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+	"network_exporter/pkg/common"
+	"network_exporter/pkg/icmp"
+)
+
+// RunMTR 执行真实的MTR路由跟踪（并发版本）
+
+// TODO: implement functions
