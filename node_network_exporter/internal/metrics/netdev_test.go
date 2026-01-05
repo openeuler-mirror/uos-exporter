a2e2bf6a80426b6d98651e265aadb6a2ecae7348
lvgenggeng <lvgenggeng@uniontech.com>
2026-01-05 17:06:00 +0800

    Add initial structure for node_network_exporter/internal/metrics/netdev_test.go

---
+++ node_network_exporter/internal/metrics/netdev_test.go
@@ -0,0 +1,12 @@
+package metrics
+
+import (
+	"net"
+	"testing"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+
+// TODO: implement functions
