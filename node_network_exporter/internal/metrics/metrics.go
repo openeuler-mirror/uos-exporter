e168948ca9d739bda5adde513553b305efd3030f
lvgenggeng <lvgenggeng@uniontech.com>
2026-01-05 16:56:00 +0800

    Add initial structure for node_network_exporter/internal/metrics/metrics.go

---
+++ node_network_exporter/internal/metrics/metrics.go
@@ -0,0 +1,18 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	Name    = "node_network_exporter"
+	Version = "1.0.0"
+)
+
+type baseMetrics struct {
+	labels []string
+	desc   *prometheus.Desc
+}
+
+
+// TODO: implement functions
