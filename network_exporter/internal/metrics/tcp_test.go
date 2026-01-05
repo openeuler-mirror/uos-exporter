bdcb15825d41fcd71d6ab15f202653ebc8f90bdb
maqi <maqi@uniontech.com>
2026-01-06 06:59:00 +0800

    Add initial structure for network_exporter/internal/metrics/tcp_test.go

---
+++ network_exporter/internal/metrics/tcp_test.go
@@ -0,0 +1,15 @@
+package metrics
+
+import (
+	"log/slog"
+	"net"
+	"os"
+	"testing"
+	
+
+	"network_exporter/config"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+
+// TODO: implement functions
