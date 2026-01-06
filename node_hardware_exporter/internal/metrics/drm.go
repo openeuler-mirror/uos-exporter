e5aacf644e98da73f1cc519ec9472c5cf801df72
wangqia <wangqia@uniontech.com>
2026-01-06 21:52:00 +0800

    Add initial structure for node_hardware_exporter/internal/metrics/drm.go

---
+++ node_hardware_exporter/internal/metrics/drm.go
@@ -0,0 +1,11 @@
+package metrics
+
+import (
+	"node_hardware_exporter/internal/exporter"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/procfs/sysfs"
+)
+
+
+// TODO: implement functions
