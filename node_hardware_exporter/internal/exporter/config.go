b690618081733d2f09762ddfe6fac19f8339c22c
wangqia <wangqia@uniontech.com>
2026-01-06 21:44:00 +0800

    Complete implementation of node_hardware_exporter/internal/exporter/config.go

--- node_hardware_exporter/internal/exporter/config.go
+++ node_hardware_exporter/internal/exporter/config.go
@@ -24,5 +24,34 @@ var (
 	}
 )
 
+func init() {
+	kingpin.HelpFlag.Short('h')
+	Configfile = kingpin.Flag("config", "Configuration file").
+		Short('c').
+		Default("/etc/uos-exporter/node-hardware-exporter.yaml").
+		String()
+}
 
-// TODO: implement functions
+type Config struct {
+	Logging     logger.Config `yaml:"log"`
+	Address     string        `yaml:"address"`
+	Port        int           `yaml:"port"`
+	MetricsPath string        `yaml:"metricsPath"`
+}
+
+func Unpack(config interface{}) error {
+	if !utils.FileExists(*Configfile) {
+		logrus.Errorf("%s file not found", *Configfile)
+	} else {
+		file, err := os.Open(*Configfile)
+		if err != nil {
+			logrus.Error("Failed to open config file: ", err)
+			return err
+		}
+		err = yaml.NewDecoder(file).Decode(config)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
