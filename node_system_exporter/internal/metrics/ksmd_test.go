32bae6ca9960b7c3278727423243143ea168d89d
yangfei <yangfei@uniontech.com>
2026-01-07 05:47:00 +0800

    Add initial structure for node_system_exporter/internal/metrics/ksmd_test.go

--- node_system_exporter/internal/metrics/ksmd_test.go
+++ node_system_exporter/internal/metrics/ksmd_test.go
@@ -5,25 +5,5 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-func TestNewKSMDCollector(t *testing.T) {
-	collector := NewKSMDCollector()
-	
-	if collector == nil {
-		t.Fatal("NewKSMDCollector returned nil")
-	}
-	
-	if collector.logger == nil {
-		t.Error("Logger should not be nil")
-	}
-}
 
-func TestKSMDCollectorImplementsCollector(t *testing.T) {
-	collector := NewKSMDCollector()
-	
-	// Test that it implements the Collector interface
-	var _ prometheus.Collector = collector
-}
-
-
-
-
+// TODO: implement functions
