33cb05c044c74f9ae1c173ae333ee52d82e07818
pangqing <pangqing@uniontech.com>
2026-01-06 16:07:00 +0800

    Complete implementation of nextdns_exporter/internal/server/request.go

--- nextdns_exporter/internal/server/request.go
+++ nextdns_exporter/internal/server/request.go
@@ -17,5 +17,24 @@ type Request struct {
 type HandlerFunc func(*Request)
 
 // NewRequest creates a new request
+func NewRequest(w http.ResponseWriter, r *http.Request) *Request {
+	return &Request{
+		W: w,
+		R: r,
+	}
+}
 
-// TODO: implement functions
+// Fail writes a failure response
+func (r *Request) Fail(status int) {
+	r.W.Header().Set("Content-Type", "text/html")
+	r.W.WriteHeader(status)
+	if r.Error != nil {
+		if _, err := r.W.Write([]byte(r.Error.Error())); err != nil {
+			fmt.Printf("The write error response failed: %v\n", err)
+		}
+	} else {
+		if _, err := r.W.Write([]byte(fmt.Sprintf("Error %d", status))); err != nil {
+			fmt.Printf("The write error response failed: %v\n", err)
+		}
+	}
+}
