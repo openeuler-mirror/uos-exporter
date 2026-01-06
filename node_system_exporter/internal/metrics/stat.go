3b8597cc35ccca5d16818f7023b07c16bb5b6b57
yangfei <yangfei@uniontech.com>
2026-01-07 06:05:00 +0800

    Add initial structure for node_system_exporter/internal/metrics/stat.go

---
+++ node_system_exporter/internal/metrics/stat.go
@@ -0,0 +1,12 @@
+package metrics
+
+import (
+	"log/slog"
+
+	"node_system_exporter/internal/exporter"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/procfs"
+)
+
+
+// TODO: implement functions
