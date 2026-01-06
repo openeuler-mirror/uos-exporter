6362bce9880927456e0f40b55ded47f50c7aa994
yangfei <yangfei@uniontech.com>
2026-01-07 04:19:00 +0800

    Add initial structure for node_system_exporter/config/config.go

---
+++ node_system_exporter/config/config.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"node_system_exporter/pkg/utils"
+	"github.com/alecthomas/kingpin"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	ScrapeUrl       *string
+	Insecure        *bool
+	DefaultSettings = Settings{
+		//ScrapeUri: "http://127.0.0.1:24220/api/plugins.json",
+		Insecure: false,
+	}
+)
+
+func init() {
+	ScrapeUrl = kingpin.Flag("scrape_uri",
+		"Scrape URI").
+		Short('s').
