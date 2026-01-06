1b1cc685725595d31fddd7d62e17dec9a053b543
yangfei <yangfei@uniontech.com>
2026-01-07 07:23:00 +0800

    Add initial structure for openldap_exporter/pkg/logger/logger.go

---
+++ openldap_exporter/pkg/logger/logger.go
@@ -0,0 +1,24 @@
+package logger
+
+import (
+	"strings"
+	"time"
+
+	formatter "gitee.com/weidongkl/logrus-formatter"
+	"github.com/sirupsen/logrus"
+)
+
+type Config struct {
+	Level   string        `yaml:"level"`
+	LogPath string        `yaml:"log_path"`
+	MaxSize string        `yaml:"max_size"`
+	MaxAge  time.Duration `yaml:"max_age"`
+}
+
+type fileLogConfig struct {
+	FileRotator *FileRotator
+	level       string
+}
+
+
+// TODO: implement functions
