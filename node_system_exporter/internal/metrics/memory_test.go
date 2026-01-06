6942a62ea7ad53caa1b07bbf271a449e2b008a65
yangfei <yangfei@uniontech.com>
2026-01-07 06:00:00 +0800

    Complete implementation of node_system_exporter/internal/metrics/memory_test.go

--- node_system_exporter/internal/metrics/memory_test.go
+++ node_system_exporter/internal/metrics/memory_test.go
@@ -5,5 +5,26 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+func TestNewMemoryCollector(t *testing.T) {
+	collector := NewMemoryCollector()
+	
+	if collector == nil {
+		t.Fatal("NewMemoryCollector returned nil")
+	}
+	
+	if collector.logger == nil {
+		t.Error("Logger should not be nil")
+	}
+	
+	// Note: fs field is a struct, not a pointer, so we can't check for nil
+}
 
-// TODO: implement functions
+func TestMemoryCollectorImplementsCollector(t *testing.T) {
+	collector := NewMemoryCollector()
+	if collector == nil {
+		t.Skip("MemoryCollector creation failed, skipping interface test")
+	}
+	
+	// Test that it implements the Collector interface
+	var _ prometheus.Collector = collector
+} 
