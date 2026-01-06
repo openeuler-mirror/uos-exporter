3636a1199d2ecfbddeaf895fe83a7a745016ac29
yangfei <yangfei@uniontech.com>
2026-01-07 05:38:00 +0800

    Complete implementation of node_system_exporter/internal/metrics/cpu_vulnerabilities.go

--- node_system_exporter/internal/metrics/cpu_vulnerabilities.go
+++ node_system_exporter/internal/metrics/cpu_vulnerabilities.go
@@ -8,5 +8,57 @@ import (
 	"github.com/prometheus/procfs/sysfs"
 )
 
+func init() {
+	exporter.Register(NewCPUVulnerabilitiesCollector())
+}
 
-// TODO: implement
+type CPUVulnerabilitiesCollector struct {
+	*baseMetrics
+	desc   *prometheus.Desc
+	logger *slog.Logger
+}
+
+func NewCPUVulnerabilitiesCollector() *CPUVulnerabilitiesCollector {
+	logger := slog.Default()
+
+	desc := prometheus.NewDesc(
+		prometheus.BuildFQName("node", "cpu_vulnerabilities", "info"),
+		"Details of each CPU vulnerability reported by sysfs. The value of the series is an int encoded state of the vulnerability. The same state is stored as a string in the label",
+		[]string{"codename", "state", "mitigation"},
+		nil,
+	)
+
+	return &CPUVulnerabilitiesCollector{
+		desc:   desc,
+		logger: logger,
+	}
+}
+
+func (c *CPUVulnerabilitiesCollector) Collect(ch chan<- prometheus.Metric) {
+	fs, err := sysfs.NewFS("/sys")
+	if err != nil {
+		c.logger.Debug("failed to open sysfs", "error", err)
+		return
+	}
+
+	vulnerabilities, err := fs.CPUVulnerabilities()
+	if err != nil {
+		c.logger.Debug("failed to get vulnerabilities", "error", err)
+		return
+	}
+
+	for _, vulnerability := range vulnerabilities {
+		ch <- prometheus.MustNewConstMetric(
+			c.desc,
+			prometheus.GaugeValue,
+			1.0,
+			vulnerability.CodeName,
+			sysfs.VulnerabilityHumanEncoding[vulnerability.State],
+			vulnerability.Mitigation,
+		)
+	}
+}
+
+func (c *CPUVulnerabilitiesCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- c.desc
+} 
