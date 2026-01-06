e1dcb505a27e49b572bbe4cea9a56900aafcee95
maqi <maqi@uniontech.com>
2026-01-06 10:24:00 +0800

    Add initial structure for smartctl_exporter/internal/server/ratelimit_middlware.go

---
+++ smartctl_exporter/internal/server/ratelimit_middlware.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"smartctl_exporter/pkg/ratelimit"
+	"github.com/alecthomas/kingpin/v2"
+	"time"
+)
+
+var (
+	rateLimitInterval *time.Duration
+	rateLimitSize     *int
+	UseRatelimit      *bool
+)
+
+func init() {
+	rateLimitInterval = kingpin.Flag("rate_limit_interval",
+		"rate limit interval").Default("1s").Duration()
+	rateLimitSize = kingpin.Flag("rate_limit_size",
+		"rate limit size").Default("100").Int()
+	UseRatelimit = kingpin.Flag("use_ratelimit",
+		"use rate limit").Bool()
+}
+
+func Ratelimit(ratelimiter *ratelimit.RateLimiter) HandlerFunc {
+	return func(req *Request) {
+		if err := ratelimiter.Get(); err != nil {
+			req.Error = err
+			req.Fail(429)
+		}
+	}
+}
