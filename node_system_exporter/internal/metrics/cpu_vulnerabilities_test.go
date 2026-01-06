6037b938e514cde0f43288c93295d95d49c7b92d
yangfei <yangfei@uniontech.com>
2026-01-07 05:40:00 +0800

    Complete implementation of node_system_exporter/internal/metrics/cpu_vulnerabilities_test.go

--- node_system_exporter/internal/metrics/cpu_vulnerabilities_test.go
+++ node_system_exporter/internal/metrics/cpu_vulnerabilities_test.go
@@ -5,5 +5,25 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+func TestNewCPUVulnerabilitiesCollector(t *testing.T) {
+	collector := NewCPUVulnerabilitiesCollector()
+	
+	if collector == nil {
+		t.Fatal("NewCPUVulnerabilitiesCollector returned nil")
+	}
+	
+	if collector.logger == nil {
+		t.Error("Logger should not be nil")
+	}
+}
 
-// TODO: implement
+func TestCPUVulnerabilitiesCollectorImplementsCollector(t *testing.T) {
+	collector := NewCPUVulnerabilitiesCollector()
+	
+	// Test that it implements the Collector interface
+	var _ prometheus.Collector = collector
+}
+
+
+
+
