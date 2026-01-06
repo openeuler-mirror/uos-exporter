7bc3fb4b008c2173ac732267b2c411440d9b8ebd
pangqing <pangqing@uniontech.com>
2026-01-06 15:04:00 +0800

    Add initial structure for keepalived_container_exporter/internal/server/server.go

---
+++ keepalived_container_exporter/internal/server/server.go
@@ -0,0 +1,46 @@
+package server
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"math"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"keepalived_container_exporter/config"
+	"keepalived_container_exporter/internal/exporter"
+	_ "keepalived_container_exporter/internal/metrics"
+	"keepalived_container_exporter/pkg/logger"
+	"keepalived_container_exporter/pkg/ratelimit"
+	"keepalived_container_exporter/pkg/utils"
+
+	"github.com/alecthomas/kingpin"
+	"github.com/dustin/go-humanize"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
+)
+
+var defaultSeverVersion = "1.0.0"
+
+type Server struct {
+	Name           string
+	Version        string
+	CommonConfig   exporter.Config
+	promReg        *prometheus.Registry
+	handlers       []HandlerFunc
+	ExitSignal     chan struct{}
+	Error          error
+	callback       sync.Once
+	ExporterConfig config.Settings
+	server         *http.Server
+}
+
+
+// TODO: implement functions
