2df8f1440ff657a12a6491f428832d0ea06635e0
maqi <maqi@uniontech.com>
2026-01-06 09:07:00 +0800

    Add initial structure for pacemaker_exporter/internal/exporter/config.go

---
+++ pacemaker_exporter/internal/exporter/config.go
@@ -0,0 +1,29 @@
+package exporter
+
+import (
+	"os"
+	"pacemaker_exporter/pkg/logger"
+	"pacemaker_exporter/pkg/utils"
+	"time"
+
+	"github.com/alecthomas/kingpin"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
+)
+
+var (
+	Configfile    *string
+	DefaultConfig = Config{
+		Logging: logger.Config{
+			Level:   "debug",
+			LogPath: "/var/log/exporter.log",
+			MaxSize: "10MB",
+			MaxAge:  time.Hour * 24 * 7},
+		Address:     "127.0.0.1",
+		Port:        8080,
+		MetricsPath: "/metrics",
+	}
+)
+
+
+// TODO: implement functions
