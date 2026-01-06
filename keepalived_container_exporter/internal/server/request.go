4da10a8a56623de66899165f91c521977623d2fd
pangqing <pangqing@uniontech.com>
2026-01-06 15:02:00 +0800

    Add initial structure for keepalived_container_exporter/internal/server/request.go

---
+++ keepalived_container_exporter/internal/server/request.go
@@ -0,0 +1,18 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Request struct {
+	Request        *http.Request
+	ResponseWriter http.ResponseWriter
+	Error          error
+	handlers       []HandlerFunc
+}
+
+type HandlerFunc func(ctx *Request)
+
+
+// TODO: implement functions
