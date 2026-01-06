36b347460a67d089c5c39178ea800f70192efc32
pangqing <pangqing@uniontech.com>
2026-01-06 14:55:00 +0800

    Complete implementation of keepalived_container_exporter/internal/metrics/vrrp.go

--- keepalived_container_exporter/internal/metrics/vrrp.go
+++ keepalived_container_exporter/internal/metrics/vrrp.go
@@ -5,5 +5,6 @@ import "github.com/hashicorp/go-version"
 var vrrpScriptStateSupportedVersion = version.Must(version.NewVersion("1.4.0"))
 
 // HasVRRPScriptStateSupport check if Keepalived version supports VRRP Script State in output.
-
-// TODO: implement functions
+func HasVRRPScriptStateSupport(v *version.Version) bool {
+	return v == nil || v.GreaterThanOrEqual(vrrpScriptStateSupportedVersion)
+}
