de1528989a2290403aa190f3f26b3a7b58cbb48b
pangqing <pangqing@uniontech.com>
2026-01-06 16:16:00 +0800

    Complete implementation of nextdns_exporter/pkg/logger/filerotator_test.go

--- nextdns_exporter/pkg/logger/filerotator_test.go
+++ nextdns_exporter/pkg/logger/filerotator_test.go
@@ -7,5 +7,50 @@ import (
 	"time"
 )
 
+func TestFileRotator(t *testing.T) {
+	basePath := "test.log"
+	maxSize := int64(100) // 100 bytes
+	maxAge := time.Second * 10
 
-// TODO: implement functions
+	fr := NewFileRotator(basePath, maxSize, maxAge)
+
+	data := []byte("Test data that should cause a rotate")
+	for i := 0; i < 10; i++ {
+		n, err := fr.Write(data)
+		if err != nil {
+			t.Errorf("Write failed: %v", err)
+		}
+		if n != len(data) {
+			t.Errorf("Write wrote %d bytes; want %d", n, len(data))
+		}
+	}
+
+	newFiles, err := os.ReadDir(".")
+	if err != nil {
+		t.Fatalf("Failed to read directory: %v", err)
+	}
+
+	logFiles := false
+	for _, file := range newFiles {
+		if file.IsDir() {
+			continue
+		}
+		if file.Name() == basePath || strings.HasPrefix(file.Name(), basePath+".20") {
+			logFiles = true
+			break
+		}
+	}
+
+	if !logFiles {
+		t.Errorf("No log files created")
+	}
+
+	for _, file := range newFiles {
+		if file.IsDir() {
+			continue
+		}
+		if file.Name() == basePath || strings.HasPrefix(file.Name(), basePath+".") {
+			os.Remove(file.Name())
+		}
+	}
+}
