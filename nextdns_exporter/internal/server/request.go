e1be455e50c1673b1c90b9f5a8ded6b42f94ce5b
pangqing <pangqing@uniontech.com>
2026-01-06 16:06:00 +0800

    Add initial structure for nextdns_exporter/internal/server/request.go

--- nextdns_exporter/internal/server/request.go
+++ nextdns_exporter/internal/server/request.go
@@ -17,24 +17,5 @@ type Request struct {
 type HandlerFunc func(*Request)
 
 // NewRequest creates a new request
-func NewRequest(w http.ResponseWriter, r *http.Request) *Request {
-	return &Request{
-		W: w,
-		R: r,
-	}
-}
 
-// Fail writes a failure response
-func (r *Request) Fail(status int) {
-	r.W.Header().Set("Content-Type", "text/html")
-	r.W.WriteHeader(status)
-	if r.Error != nil {
-		if _, err := r.W.Write([]byte(r.Error.Error())); err != nil {
-			fmt.Printf("The write error response failed: %v\n", err)
-		}
-	} else {
-		if _, err := r.W.Write([]byte(fmt.Sprintf("Error %d", status))); err != nil {
-			fmt.Printf("The write error response failed: %v\n", err)
-		}
-	}
-}
+// TODO: implement functions
