88180ea194119ecb495f3b02ee6271a9774ccd7f
maqi <maqi@uniontech.com>
2026-01-06 08:16:00 +0800

    Add initial structure for newrelic_exporter/internal/metrics/mock_test.go

---
+++ newrelic_exporter/internal/metrics/mock_test.go
@@ -0,0 +1,16 @@
+package metrics
+
+import (
+	"testing"
+	"time"
+	
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/sirupsen/logrus"
+	
+	"newrelic_exporter/internal/exporter"
+	"newrelic_exporter/pkg/newrelic"
+)
+
+// 设置测试环境
+
+// TODO: implement functions
