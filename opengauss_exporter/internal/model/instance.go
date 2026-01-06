aca0fcc8e4659d6be9ac9c18e9e262415bc83afa
wangqia <wangqia@uniontech.com>
2026-01-06 23:41:00 +0800

    Add initial structure for opengauss_exporter/internal/model/instance.go

---
+++ opengauss_exporter/internal/model/instance.go
@@ -0,0 +1,12 @@
+package model
+
+import (
+	"fmt"
+	"strings"
+	// "github.com/jackc/pgx/v4/stdlib"
+)
+
+// InstanceConnection 表示 OpenGauss 实例的连接信息
+type InstanceConnection struct {
+
+// TODO: implement functions
