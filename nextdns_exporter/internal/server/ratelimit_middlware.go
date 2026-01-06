da83fb7e0edc838486af974455a28cba70608195
pangqing <pangqing@uniontech.com>
2026-01-06 16:04:00 +0800

    Add initial structure for nextdns_exporter/internal/server/ratelimit_middlware.go

--- nextdns_exporter/internal/server/ratelimit_middlware.go
+++ nextdns_exporter/internal/server/ratelimit_middlware.go
@@ -13,20 +13,5 @@ var (
 	UseRatelimit      *bool
 )
 
-func init() {
-	rateLimitInterval = kingpin.Flag("rate_limit_interval",
-		"rate limit interval").Default("1s").Duration()
-	rateLimitSize = kingpin.Flag("rate_limit_size",
-		"rate limit size").Default("100").Int()
-	UseRatelimit = kingpin.Flag("use_ratelimit",
-		"use rate limit").Bool()
-}
 
-func Ratelimit(ratelimiter *ratelimit.RateLimiter) HandlerFunc {
-	return func(req *Request) {
-		if !ratelimiter.Allow() {
-			req.Error = fmt.Errorf("rate limit exceeded")
-			req.Fail(429)
-		}
-	}
-}
+// TODO: implement functions
