c03218c8860d053be8bd45fcb5bec845c7545c52
yangxiaoxuan <yangxiaoxuan@uniontech.com>
2026-01-07 15:18:00 +0800

    Complete implementation of snmp_exporter/pkg/utils/http.go

--- snmp_exporter/pkg/utils/http.go
+++ snmp_exporter/pkg/utils/http.go
@@ -5,5 +5,13 @@ import (
 	"net/url"
 )
 
-
-// TODO: implement functions
+func ValidateURI(uri string) error {
+	parsedURL, err := url.ParseRequestURI(uri)
+	if err != nil {
+		return fmt.Errorf("invalid URI format: %w", err)
+	}
+	if parsedURL.Scheme != "http" && parsedURL.Scheme != "https" {
+		return fmt.Errorf("unsupported scheme: %s", parsedURL.Scheme)
+	}
+	return nil
+}
