733767bcf7c76652fa4686ddcc9eaeb7c7aef277
zhuhongbo <zhuhongbo@uniontech.com>
2026-01-08 06:44:00 +0800

    Add initial structure for systemd_resolved_exporter/internal/metrics/dnssec.go

---
+++ systemd_resolved_exporter/internal/metrics/dnssec.go
@@ -0,0 +1,11 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/sirupsen/logrus"
+	"systemd_resolved_exporter/internal/exporter"
+	"systemd_resolved_exporter/internal/systemd"
+)
+
+
+// TODO: implement functions
