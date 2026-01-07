33758db11236678c1ad363d64b474f9c05373036
yangxiaoxuan <yangxiaoxuan@uniontech.com>
2026-01-07 15:42:00 +0800

    Complete implementation of lvm_exporter/internal/metrics/lvm_exporter.go

--- lvm_exporter/internal/metrics/lvm_exporter.go
+++ lvm_exporter/internal/metrics/lvm_exporter.go
@@ -6,5 +6,37 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+func init() {
+	exporter.Register(
+		NewLvmExporter())
+}
 
-// TODO: implement functions
+type LvmExporter struct {
+	pvexporter    *PvExporter
+	vgexporter    *VgExporter
+	lvexporter    *LvExporter
+	pvsegexporter *PvSegExporter
+}
+
+func NewLvmExporter() *LvmExporter {
+	return &LvmExporter{
+		pvexporter:    NewPvExporter(),
+		vgexporter:    NewVgExporter(),
+		lvexporter:    NewLvExporter(),
+		pvsegexporter: NewPvSegExporter(),
+	}
+}
+
+func (e *LvmExporter) Describe(ch chan<- *prometheus.Desc) {
+	e.pvexporter.Describe(ch)
+	e.vgexporter.Describe(ch)
+	e.lvexporter.Describe(ch)
+	e.pvsegexporter.Describe(ch)
+}
+
+func (e *LvmExporter) Collect(ch chan<- prometheus.Metric) {
+	e.pvexporter.Collect(ch)
+	e.vgexporter.Collect(ch)
+	e.lvexporter.Collect(ch)
+	e.pvsegexporter.Collect(ch)
+}
