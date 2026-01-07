9a0e87f947ec09f0b7db2f0fd5015318d014ec54
zhuhongbo <zhuhongbo@uniontech.com>
2026-01-08 05:55:00 +0800

    Add initial structure for prometheus_paperless_exporter/internal/metrics/warning.go

--- prometheus_paperless_exporter/internal/metrics/warning.go
+++ prometheus_paperless_exporter/internal/metrics/warning.go
@@ -16,14 +16,5 @@ type warning struct {
 
 var _ prometheus.Metric = (*warning)(nil)
 
-func newWarning(err error) *warning {
-	return &warning{err}
-}
 
-func (*warning) Desc() *prometheus.Desc {
-	return nil
-}
-
-func (*warning) Write(*dto.Metric) error {
-	return os.ErrInvalid
-}
+// TODO: implement functions
