685d73bbd7ff64ebe69f161bc3b7682d61b5598a
zhuhongbo <zhuhongbo@uniontech.com>
2026-01-08 05:14:00 +0800

    Add initial structure for prometheus_paperless_exporter/exporter.go

---
+++ prometheus_paperless_exporter/exporter.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"prometheus_paperless_exporter/internal/server"
+	"prometheus_paperless_exporter/pkg/logger"
+	"github.com/sirupsen/logrus"
+)
+
+
+// TODO: implement functions
