865e8e51e9756fe7c5d3a00003a73a99cf7460c7
yangfei <yangfei@uniontech.com>
2026-01-07 12:23:57 +0800

    Add redis_exporter/pkg/utils/signal.go

---
+++ redis_exporter/pkg/utils/signal.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"github.com/sirupsen/logrus"
+)
+
+func HandleSignals(function func()) {
+	var callback sync.Once
+	sigc := make(chan os.Signal, 1)
+	defer close(sigc)
+	signal.Notify(sigc,
+		syscall.SIGINT,
+		syscall.SIGTERM)
+
+	sig := <-sigc
+	logrus.Infof("service received signal: %v", sig)
+	callback.Do(function)
+}
