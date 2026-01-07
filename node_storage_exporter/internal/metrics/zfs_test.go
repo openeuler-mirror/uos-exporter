4f881140a86cdc8333cfe6e82552815e5816c52d
zhangshaoning <zhangshaoning@uniontech.com>
2026-01-07 22:23:00 +0800

    Add initial structure for node_storage_exporter/internal/metrics/zfs_test.go

---
+++ node_storage_exporter/internal/metrics/zfs_test.go
@@ -0,0 +1,11 @@
+package metrics
+
+import (
+	"testing"
+	"strings"
+	"github.com/prometheus/client_golang/prometheus"
+	dto "github.com/prometheus/client_model/go"
+)
+
+
+// TODO: implement functions
