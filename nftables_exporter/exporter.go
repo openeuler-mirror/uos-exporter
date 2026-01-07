29ad02581752cefc9b1281ca97854e50db72c861
zhuhongbo <zhuhongbo@uniontech.com>
2026-01-08 04:23:00 +0800

    Complete implementation of nftables_exporter/exporter.go

--- nftables_exporter/exporter.go
+++ nftables_exporter/exporter.go
@@ -6,5 +6,29 @@ import (
 	"nftables_exporter/pkg/logger"
 )
 
+func Run(name string, version string) error {
+	logger.InitDefaultLog()
+	s := server.NewServer(name, version)
 
-// TODO: implement functions
+	s.PrintVersion()
+	err := s.SetUp()
+	if err != nil {
+		logrus.Errorf("SetUp error: %v", err)
+		return err
+	}
+	go func() {
+		err := s.Run()
+		if err != nil {
+			logrus.Errorf("Run error: %v", err)
+			s.Error = err
+		}
+
+		s.Exit()
+	}()
+	select {
+	case <-s.ExitSignal:
+		s.Stop()
+		logrus.Info("Exit exporter server completed")
+		return s.Error
+	}
+}
