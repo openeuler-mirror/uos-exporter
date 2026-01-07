c78d5ab9c7be13dff0f2bbf3c9546ab3ed39dc1b
zhangshaoning <zhangshaoning@uniontech.com>
2026-01-07 20:33:00 +0800

    Add initial structure for mysqld_exporter/config/config.go

---
+++ mysqld_exporter/config/config.go
@@ -0,0 +1,26 @@
+package config
+
+var (
+	defaultMysqlSettings = MysqlSettings{
+		Host:     "127.0.0.1",
+		Port:     3306,
+		User:     "root",
+		Password: "root",
+		Protocol: "tcp",
+	}
+	DefaultSettings = Settings{
+		Mysql: defaultMysqlSettings,
+	}
+)
+
+type MysqlSettings struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	Protocol string `yaml:"protocol"`
+}
+
+type Settings struct {
+
+// TODO: implement
