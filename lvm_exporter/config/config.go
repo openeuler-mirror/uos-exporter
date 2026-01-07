f1156e6482227a29ea51b2b094328d060f934d45
yangxiaoxuan <yangxiaoxuan@uniontech.com>
2026-01-07 15:27:00 +0800

    Add initial structure for lvm_exporter/config/config.go

---
+++ lvm_exporter/config/config.go
@@ -0,0 +1,20 @@
+package config
+
+import (
+	"lvm_exporter/pkg/utils"
+
+	"github.com/alecthomas/kingpin"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	ScrapeUrl       *string
+	Insecure        *bool
+	DefaultSettings = Settings{
+		//ScrapeUri: "http://127.0.0.1:24220/api/plugins.json",
+		Insecure: false,
+	}
+)
+
+
+// TODO: implement functions
