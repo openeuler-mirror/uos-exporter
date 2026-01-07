09ffdbf91583118b7179efdff56277f9f52a70f7
zhuhongbo <zhuhongbo@uniontech.com>
2026-01-08 06:51:00 +0800

    Complete implementation of systemd_resolved_exporter/internal/metrics/transactions.go

--- systemd_resolved_exporter/internal/metrics/transactions.go
+++ systemd_resolved_exporter/internal/metrics/transactions.go
@@ -7,5 +7,33 @@ import (
 	"systemd_resolved_exporter/internal/systemd"
 )
 
+func init() {
+	exporter.Register(
+		NewTransactions("transactions_info",
+			"transactions info",
+			[]string{"type"}))
+}
 
-// TODO: implement functions
+type Transactions struct {
+	*baseMetrics
+}
+
+func NewTransactions(fqname, help string, labels []string) *Transactions {
+	return &Transactions{NewMetrics(fqname, help, labels)}
+}
+
+func (c *Transactions) Collect(ch chan<- prometheus.Metric) {
+	stats := systemd.GetSystemdResolvedStats()
+	value, ok := stats["Current Transactions"]
+	if !ok {
+		logrus.Errorf("get Current Transactions failed")
+	} else {
+		c.baseMetrics.collect(ch, value, []string{"current"})
+	}
+	value, ok = stats["Total Transactions"]
+	if !ok {
+		logrus.Errorf("get Total Transactions failed")
+	} else {
+		c.baseMetrics.collect(ch, value, []string{"total"})
+	}
+}
