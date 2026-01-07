59bba7714a226b33d098c95b66dd9dcf8df458ee
zhangshaoning <zhangshaoning@uniontech.com>
2026-01-07 22:55:00 +0800

    Add initial structure for ssl_exporter/config/config.go

---
+++ ssl_exporter/config/config.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"time"
+
+	"ssl_exporter/pkg/utils"
+
+	"github.com/alecthomas/kingpin"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	ScrapeUrl       *string
+	Insecure        *bool
+	DefaultSettings = Settings{
+		//ScrapeUri: "http://127.0.0.1:24220/api/plugins.json",
+		Insecure: false,
+	}
+)
+
+
+// TODO: implement functions
