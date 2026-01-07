2f2d73903eb288d36cf655b67d940d31d07eb327
yangxiaoxuan <yangxiaoxuan@uniontech.com>
2026-01-07 15:02:00 +0800

    Add initial structure for snmp_exporter/internal/server/server.go

---
+++ snmp_exporter/internal/server/server.go
@@ -0,0 +1,47 @@
+package server
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"math"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"snmp_exporter/config"
+	"snmp_exporter/internal/exporter"
+	metrics "snmp_exporter/internal/metrics"
+	"snmp_exporter/pkg/logger"
+	"snmp_exporter/pkg/ratelimit"
+	"snmp_exporter/pkg/utils"
+
+	"github.com/alecthomas/kingpin"
+	"github.com/dustin/go-humanize"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/exporter-toolkit/web"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
+)
+
+var defaultSeverVersion = "1.0.0"
+
+type Server struct {
+	Name           string
+	Version        string
+	CommonConfig   exporter.Config
+	promReg        *prometheus.Registry
+	handlers       []HandlerFunc
+	ExitSignal     chan struct{}
+	Error          error
+	callback       sync.Once
+	ExporterConfig config.Settings
+	server         *http.Server
+}
+
+
+// TODO: implement functions
