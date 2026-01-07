d1771c459c8c39a89301c307775a7f2e5584b86d
yangxiaoxuan <yangxiaoxuan@uniontech.com>
2026-01-07 13:10:00 +0800

    Complete implementation of tc_exporter/internal/metrics/info.go

--- tc_exporter/internal/metrics/info.go
+++ tc_exporter/internal/metrics/info.go
@@ -6,5 +6,29 @@ import (
 	"tc_exporter/version"
 )
 
+func init() {
+	exporter.Register(
+		NewBuildInfo("exporter_build_info",
+			"exporter build info",
+			[]string{"version",
+				"revision",
+				"branch",
+				"goversion"}))
+}
 
-// TODO: implement
+type BuildInfo struct {
+	*baseMetrics
+}
+
+func NewBuildInfo(fqname, help string, labels []string) *BuildInfo {
+	return &BuildInfo{NewMetrics(fqname, help, labels)}
+}
+
+func (c *BuildInfo) Collect(ch chan<- prometheus.Metric) {
+	c.baseMetrics.collect(ch,
+		1,
+		[]string{version.Version,
+			version.Revision,
+			version.Branch,
+			version.GoVersion})
+}
